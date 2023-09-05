2d8048d339c215c8230c3b9c764f2e5aea35473b
Ivan Mitrichev <imitrichev@users.noreply.github.com>
2023-09-05 22:29:03 +0300

    Update device.cpp
    added main with streams

--- device.cpp
+++ device.cpp
@@ -17,3 +17,24 @@ class Stream
       double getMassFlow(){return mass_flow;}
       void print(){cout<<"Stream "<<getName()<<" "<<getMassFlow()<<endl;}
 };
+
+class Stream
+{
+    private:
+      double mass_flow;
+      string name;
+    public:
+      Stream(int s){setName("s"+std::to_string(s));}
+      void setName(string s){name=s;}
+      string getName(){return name;}
+      void setMassFlow(double m){mass_flow=m;}
+      double getMassFlow(){return mass_flow;}
+      void print(){cout<<"Stream "<<getName()<<" flow = "<<getMassFlow()<<endl;}
+};
+
+int main()
+{
+    Stream s1,s2,s3;
+    s1.setMassFlow(10.0);
+    s2.setMassFlow(5.0);
+}
