4d6460b1477aab33573638ff19c72d08c26316d2
Dusalin Amal <dekart416@yandex-team.ru>
2023-09-06 12:25:42 +0300

    add tests

--- device.cpp
+++ device.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int streamcounter;
 
 const int MIXER_OUTPUTS = 1;
+const float POSSIBLE_ERROR = 0.01;
 
 class Stream
 {
@@ -43,13 +44,13 @@ class Mixer: public Device
       }
       void addInput(shared_ptr<Stream> s) {
         if (inputs.size() == _inputs_count) {
-          throw "Too much inputs";
+          throw "Too much inputs"s;
         }
         inputs.push_back(s);
       }
       void addOutput(shared_ptr<Stream> s) {
         if (outputs.size() == MIXER_OUTPUTS) {
-          throw "Too much outputs";
+          throw "Too much outputs"s;
         }
         outputs.push_back(s);
       }
@@ -59,7 +60,7 @@ class Mixer: public Device
           sum_mass_flow += input_stream -> getMassFlow();
         }
 
-        double output_mass = sum_mass_flow / inputs.size(); // divide 0
+        double output_mass = sum_mass_flow / outputs.size(); // divide 0
 
         for (auto& output_stream : outputs) {
           output_stream -> setMassFlow(output_mass);
@@ -67,8 +68,7 @@ class Mixer: public Device
       }
 };
 
-int main()
-{
+void shouldSetOutputsCorrectlyWithOneOutput() {
     streamcounter=0;
     Mixer d1 = Mixer(2);
     
@@ -84,5 +84,76 @@ int main()
 
     d1.updateOutputs();
 
-    s3 -> print();
+    if (s3->getMassFlow() - 15 < POSSIBLE_ERROR) {
+      cout << "Test 1 passed"s << endl;
+    } else {
+      cout << "Test 1 failed"s << endl;
+    }
+}
+
+void shouldCorrectOutputs() {
+    streamcounter=0;
+    Mixer d1 = Mixer(2);
+    
+    shared_ptr<Stream> s1(new Stream(++streamcounter));
+    shared_ptr<Stream> s2(new Stream(++streamcounter));
+    shared_ptr<Stream> s3(new Stream(++streamcounter));
+    shared_ptr<Stream> s4(new Stream(++streamcounter));
+    s1->setMassFlow(10.0);
+    s2->setMassFlow(5.0);
+
+    d1.addInput(s1);
+    d1.addInput(s2);
+    d1.addOutput(s3);
+
+    try {
+      d1.addOutput(s4);
+    } catch (const string ex) {
+      if (ex == "Too much outputs"s) {
+        cout << "Test 2 passed"s << endl;
+
+        return;
+      }
+    }
+
+    cout << "Test 2 failed"s << endl;
+}
+
+void shouldCorrectInputs() {
+    streamcounter=0;
+    Mixer d1 = Mixer(2);
+    
+    shared_ptr<Stream> s1(new Stream(++streamcounter));
+    shared_ptr<Stream> s2(new Stream(++streamcounter));
+    shared_ptr<Stream> s3(new Stream(++streamcounter));
+    shared_ptr<Stream> s4(new Stream(++streamcounter));
+    s1->setMassFlow(10.0);
+    s2->setMassFlow(5.0);
+
+    d1.addInput(s1);
+    d1.addInput(s2);
+    d1.addOutput(s3);
+
+    try {
+      d1.addInput(s4);
+    } catch (const string ex) {
+      if (ex == "Too much inputs"s) {
+        cout << "Test 3 passed"s << endl;
+
+        return;
+      }
+    }
+
+    cout << "Test 3 failed"s << endl;
+}
+
+void tests() {
+  shouldSetOutputsCorrectlyWithOneOutput();
+  shouldCorrectOutputs();
+  shouldCorrectInputs();
+}
+
+int main()
+{
+    tests();
 }
