6502d030a3e9505349d4001d10288695b960c788
djotyan <djotyan@Larix.ru>
2025-09-25 22:58:17 +0300

    Added masses

--- Separator.cpp
+++ Separator.cpp
@@ -15,5 +15,8 @@ class Separator : public Device {
                   if (outputs.size() != outputAmount) {
                         throw "OUTPUT STREAM LIMIT!";
                   }
+
+                  double inputMass = inputs.at(0)->getMassFlow();
+                  double halfMass = inputMass / 2.0;
             }
 };
