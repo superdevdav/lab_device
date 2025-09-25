7be849a994fa024bbcbe9f9895b6523aac3f9a42
djotyan <djotyan@Larix.ru>
2025-09-25 22:46:17 +0300

    Added inputs and outputs amounts

--- Separator.cpp
+++ Separator.cpp
@@ -1,6 +1,8 @@
 #include "device.cpp"
 using namespace std;
 
-class Separator : public Device
-{
+class Separator : public Device {
+      private:
+            int _inputs_count = 1;
+            int _outputs_count = 2;
 };
